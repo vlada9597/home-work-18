bf00e1d56caffc5836d2304ea36657397e14c7cc
vlada9597 <vladazoloto@icloud.com>
2025-04-30 18:13:17 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+console.log('JS #1. Домашнє завдання. Основи JavaScript: Працюємо зі змінними, типами даних');
+
+
+var myNum = 10;
+console.log(myNum);
+
+var myStr = 'some string';
+console.log(myStr);
+
+var myBool = true;
+console.log(myBool);
+
+var myArr = [1, 2, 3, 4, 5];
+console.log(myArr);
+
+var myObj = {
+  first: 'First Name',
+  last: 'Last Name'
+};
+console.log(myObj);
+
+
+var decimal2 = myNum.toFixed(2);
+console.log(decimal2);
+
+
+var myBigInt = 123n;
+myBigInt = myBigInt + 1n;
+console.log(myBigInt);
